da970130559c6a0906cad30c55fbd54ad6c8f2cf
Asheze1127 <m.k.h11272005@gmail.com>
2025-09-17 10:40:52 +0900

    varidation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { prisma } from "@/lib/prisma";
-import { compare } from "bcrypt";
+import { compare } from "bcryptjs";
 
 export const authOptions = {
   providers: [
